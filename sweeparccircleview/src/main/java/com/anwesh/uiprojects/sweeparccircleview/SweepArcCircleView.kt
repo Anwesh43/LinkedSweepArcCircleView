c4ffbaec9a4baab7d4a26e03e6281f90ef263e60
Anwesh43 <anweshthecool0@gmail.com>
2019-10-03 10:40:16 +0530

    adding SweepArcCircle class

--- sweeparccircleview/src/main/java/com/anwesh/uiprojects/sweeparccircleview/SweepArcCircleView.kt
+++ sweeparccircleview/src/main/java/com/anwesh/uiprojects/sweeparccircleview/SweepArcCircleView.kt
@@ -165,7 +165,31 @@ class SweepArcCircleView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class SweepArcCircle(var i : Int) {
+
+        private val root : SACNode = SACNode(0)
+        private var curr : SACNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
