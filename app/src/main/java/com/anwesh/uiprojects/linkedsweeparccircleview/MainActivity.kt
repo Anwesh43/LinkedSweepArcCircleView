147fac7b10cd642b52001e7e6463dc783f3b3705
Anwesh43 <anweshthecool0@gmail.com>
2019-10-03 10:46:44 +0530

    adding view in activity

---
+++ app/src/main/java/com/anwesh/uiprojects/linkedsweeparccircleview/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.anwesh.uiprojects.linkedsweeparccircleview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.anwesh.uiprojects.sweeparccircleview.SweepArcCircleView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SweepArcCircleView.create(this)
+    }
+}
